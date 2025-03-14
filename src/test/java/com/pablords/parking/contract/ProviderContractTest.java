213f4e63984e943961f72e3c4598a4e8ecf5b351
Pablo Ruan dos Santos <pablo.santos@telefonicati.onmicrosoft.com>
2025-03-14 14:11:12 -0300

    refactor: contract tests

--- src/test/java/com/pablords/parking/contract/ProviderContractTest.java
+++ src/test/java/com/pablords/parking/contract/ProviderContractTest.java
@@ -36,7 +36,7 @@ import com.pablords.parking.core.ports.outbound.repositories.CheckoutRepositoryP
 import com.pablords.parking.core.ports.outbound.repositories.SlotRepositoryPort;
 import com.pablords.parking.core.valueobjects.Plate;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @Provider("ParkingService")
 @PactFolder("src/test/resources/contracts")
 // Caminho para os contratos "mockados"
@@ -68,30 +68,41 @@ class ProviderContractTest {
   Car car;
 
   @BeforeEach
-  void setup(PactVerificationContext context) {
+  void setup() {
     car = new Car(new Plate("ABC1234"), "Toyota", "Red", "Sedan");
     car.setId(UUID.fromString("f5d4b3b4-1b4b-4b4b-8b4b-4b4b4b4b4b4b"));
-    when(carRepositoryPort.findByPlate("ABC1234"))
-        .thenReturn(Optional.of(car));
-    when(carRepositoryPort.save(any(Car.class)))
-        .thenReturn(car);
-
     slot = new Slot(1l, false);
-    when(slotRepositoryPort.findAvailableSlot())
-        .thenReturn(Optional.of(slot));
-    when(slotRepositoryPort.save(slot))
-        .thenReturn(slot);
-
     checkin = new Checkin(slot, car);
     checkin.setId(UUID.fromString("f5d4b3b4-1b4b-4b4b-8b4b-4b4b4b4b4b4b"));
     checkin.setCheckInTime(LocalDateTime.parse("2025-03-13T13:00:00"));
     checkin.setCheckOutTime(LocalDateTime.parse("2025-03-13T14:00:00"));
 
+  }
+
+  @State("Existe um carro cadastrado com a placa ABC1234")
+  public void setupCar() {
+    System.out.println("Configurando o estado: Carro cadastrado no sistema");
+    when(carRepositoryPort.findByPlate("ABC1234"))
+        .thenReturn(Optional.of(car));
+    when(carRepositoryPort.save(any(Car.class)))
+        .thenReturn(car);
+  }
+
+  @State("Existe um carro cadastrado com a placa ABC1234 e uma vaga disponível")
+  public void setupCheckin() {
+    System.out.println("Configurando o estado: Carro cadastrado com a placa ABC1234 e uma vaga disponível");
+    when(carRepositoryPort.findByPlate("ABC1234"))
+        .thenReturn(Optional.of(car));
+    when(carRepositoryPort.save(any(Car.class)))
+        .thenReturn(car);
     when(checkinRepositoryPort.findByPlate("ABC1234"))
         .thenReturn(Optional.of(checkin));
     when(checkinRepositoryPort.save(any(Checkin.class)))
         .thenReturn(checkin);
-
+    when(slotRepositoryPort.findAvailableSlot())
+        .thenReturn(Optional.of(slot));
+    when(slotRepositoryPort.save(slot))
+        .thenReturn(slot);
     // Definir um horário fixo para check-in e check-out
     Instant fixedInstant = Instant.parse("2025-03-13T14:00:00Z");
     ZoneId zoneId = ZoneId.of("UTC");
@@ -103,23 +114,39 @@ class ProviderContractTest {
         .thenReturn(Optional.of(checkout));
     when(checkoutRepositoryPort.save(any(Checkout.class)))
         .thenReturn(checkout);
-
-    context.setTarget(new HttpTestTarget("localhost", port));
   }
 
-  @State("Existe um carro cadastrado com a placa ABC1234")
-  public void setupCar() {
-    System.out.println("Configurando o estado: Carro cadastrado no sistema");
+  @State("Existe um carro está estacionado com a placa ABC1234 e pronto para checkout")
+  public void setupCheckout() {
+    System.out.println("Configurando o estado: Carro está estacionado com a placa ABC1234 e pronto para checkout");
+    when(carRepositoryPort.findByPlate("ABC1234"))
+        .thenReturn(Optional.of(car));
+    when(carRepositoryPort.save(any(Car.class)))
+        .thenReturn(car);
+    when(checkinRepositoryPort.findByPlate("ABC1234"))
+        .thenReturn(Optional.of(checkin));
+    when(checkinRepositoryPort.save(any(Checkin.class)))
+        .thenReturn(checkin);
+    // Definir um horário fixo para check-in e check-out
+    Instant fixedInstant = Instant.parse("2025-03-13T14:00:00Z");
+    ZoneId zoneId = ZoneId.of("UTC");
+    Clock fixedClock = Clock.fixed(fixedInstant, zoneId);
+
+    checkout = new Checkout(checkin, fixedClock);
+    checkout.setParkingFee(2.5);
+    when(checkoutRepositoryPort.findByCheckinId(UUID.fromString("f5d4b3b4-1b4b-4b4b-8b4b-4b4b4b4b4b4b")))
+        .thenReturn(Optional.of(checkout));
+    when(checkoutRepositoryPort.save(any(Checkout.class)))
+        .thenReturn(checkout);
   }
 
-  @State("Existe um carro estacionado com a placa ABC1234")
-  public void setupCheckin() {
-    System.out.println("Configurando o estado: Carro já está estacionado");
-  }
+
+
 
   @TestTemplate
   @ExtendWith(PactVerificationInvocationContextProvider.class)
   void validatePactContract(PactVerificationContext context) {
+    context.setTarget(new HttpTestTarget("localhost", port));
     context.verifyInteraction();
   }
 }
